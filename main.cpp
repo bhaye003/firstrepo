a886c59e58a60bdaef3db1f0b220c69ac6553925
Bradley Hayes <bhaye003@hammer.cs.ucr.edu>
2017-10-04 18:49:08 -0700

    added user input

--- main.cpp
+++ main.cpp
@@ -2,7 +2,9 @@
 
 int main()
 {
-	std::cout << "hello git" << std::endl;
+	std::cout << "What is your name?" << std::endl;
+	std::cin >> name;
+	std:: << "Hello " << name << "!" << std::endl;
 	return 1;
 }
 
